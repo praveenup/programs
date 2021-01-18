40c7394034f131f91f3c4d6520ce2d394670f935
praveenup <prakum.633@gmail.com>
2021-01-19 00:40:12 +0530

    Added Stack LinkedList code

---
+++ src/com/programs/datastructure/linkedlist/LinkedListNode.java
@@ -0,0 +1,11 @@
+package com.programs.datastructure.linkedlist;
+
+public class LinkedListNode {
+    int data;
+    LinkedListNode next;
+
+    LinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
--- src/com/programs/datastructure/linkedlist/Stack.java
+++ src/com/programs/datastructure/linkedlist/Stack.java
@@ -2,11 +2,42 @@ package com.programs.datastructure.linkedlist;
 
 public class Stack {
 
-    public void push(int data){
+    LinkedListNode top;
 
+    Stack(){
+        top = null;
+    }
+
+    public void push(int data){
+        LinkedListNode tempNode = new LinkedListNode(data);
+        tempNode.next = top;
+        top = tempNode;
+    }
+
+    public void pop(){
+        top = top.next;
+    }
+
+    public void printStack(Stack stack){
+        LinkedListNode node = stack.top;
+        System.out.println("Printing:");
+        while(node!=null){
+            System.out.println(node.data);
+            node = node.next;
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.printStack(stack);
+        stack.pop();
+        stack.printStack(stack);
+        stack.push(4);
+        stack.printStack(stack);
+        stack.pop();
+        stack.printStack(stack);
     }
 }
